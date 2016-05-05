75454476269f278405c1425ac5d916c1da9de0e3
sikavinraj <sikavinraj@gmail.com>
2016-05-05 20:48:36 +0530

    Ingredient page layout implemented

--- page-ingregident.php
+++ page-ingregident.php
@@ -18,43 +18,42 @@ get_header(); ?>
            <?php
              $taxonomyName = "product_cat";
 //This gets top layer terms only.  This is done by setting parent to 0.  
-    $parent_terms = get_terms($taxonomyName, array('parent' => 0, 'orderby' => 'slug', 'hide_empty' => false));
+    $parent_terms = get_terms($taxonomyName, array('parent' => 0, 'orderby' => 'ASC', 'hide_empty' => false));
 
-    echo '<ul>';
+    echo '<ul class="ingredient-cover">';
     foreach ($parent_terms as $pterm) {
 
-        //show parent categories
-        echo '<li>
-        <a href="' . get_term_link($pterm->name, $taxonomyName) . '">' . $pterm->name . '';
-         echo '<p>'. $pterm->description . '</p>'; 
- $thumb_src = get_term_meta($pterm->term_id, 'ingredient-code', true);
+       $thumbnail_id = get_woocommerce_term_meta( $pterm->term_id, 'thumbnail_id', true );
+
+// get the image URL
+$image = wp_get_attachment_url( $thumbnail_id );
+//color code
+        $thumb_src = get_term_meta($pterm->term_id, 'ingredient-code', true);
 if($thumb_src != '') :
-	echo $thumb_src;
+
+        // print the IMG HTML for parent category
+      // echo "<div src='$image' alt='' width='400' height='400' />";
+
+        //show parent categories ?>
+        <li class="ingredient-list" style="background-image:url( <?php echo $image; ?>); ">
+            <div class="borderk">
+
+<?php
+        echo '<span class="colorbar" style="background-color:'.$thumb_src.'"></span>';
+        echo '<a href="' . get_term_link($pterm->name, $taxonomyName) . '">' . $pterm->name . '';
+         echo '<p>'. $pterm->description . '</p>';
+echo '</div>';
+
 endif;
 
-        $thumbnail_id = get_woocommerce_term_meta($pterm->term_id, 'thumbnail_id', true);
-        // get the image URL for parent category
-        $image = wp_get_attachment_url($thumbnail_id);
-        // print the IMG HTML for parent category
-        echo "<img src='{$image}' alt='' width='400' height='400' />";
 
-        //Get the Child terms
-        $terms = get_terms($taxonomyName, array('parent' => $pterm->term_id, 'orderby' => 'slug', 'hide_empty' => false));
-        foreach ($terms as $term) {
 
-            echo '<li><a href="' . get_term_link($term->name, $taxonomyName) . '">' . $term->name . '</a> </li>';
-
-            $thumbnail_id = get_woocommerce_term_meta($pterm->term_id, 'thumbnail_id', true);
-            // get the image URL for child category
-            $image = wp_get_attachment_url($thumbnail_id);
-            // print the IMG HTML for child category
-            echo "<img src='{$image}' alt='' width='400' height='400' />";
 
 
         }
         
        echo '  </a></li>';
-    }
+
     echo '</ul>';
            ?>
 <!-- ingrediant -->
